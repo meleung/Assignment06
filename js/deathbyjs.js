d480f1ab9ab8a55e936664652318b09bd2495c74
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 21:05:51 -0700

    deathbyjs.js: Complete Step 2

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -11,6 +11,24 @@
 //window.console.log("You entered: " + input + "\nResult: " + output);
 
 //STEP 2
+//function capitalize(input) {
+//    "use strict";
+//    var result, i;
+//    result = input.split(" ");
+//    for (i = 0; i < result.length; i += 1) {
+//        if (result[i].length === 1) {
+//            result[i] = result[i][0].toUpperCase();
+//            
+//        } else if (result[i].length > 1) {
+//            result[i] = result[i][0].toUpperCase() + result[i].substr(1);
+//        }
+//    }
+//    return result.join(" ");
+//}
+//var input, output;
+//input = window.prompt("Enter a string");
+//output = capitalize(input);
+//window.console.log("You entered: " + input + "\nResult: " + output);
 
 //STEP 3
 
