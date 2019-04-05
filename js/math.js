1ca1c30c00bff8aa0f72bd88e3f859cc4e62cf4c
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 19:39:01 -0700

    math.js: Complete Step 1

--- js/math.js
+++ js/math.js
@@ -1,6 +1,12 @@
 /*eslint-env browser*/
 
 //STEP 1
+//var n = parseInt(window.prompt("Please enter a number"), 10);
+//if (isNaN(n)) {
+//    window.console.log("Invalid number.");
+//} else {
+//    window.console.log("The absolute value of " + n + " is " + Math.abs(n) + ".");
+//}
 
 //STEP 2
 
