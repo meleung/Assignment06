88ad65ce976520e1f5baa566501ae70826ce7073
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 20:58:35 -0700

    deathbyjs.js: Complete Step 1

--- js/deathbyjs.js
+++ js/deathbyjs.js
@@ -1,6 +1,14 @@
 /*eslint-env browser*/
 
 //STEP 1
+//function alphabetize(input) {
+//    "use strict";
+//    return input.split("").sort().join("");
+//}
+//var input, output;
+//input = window.prompt("Enter a string");
+//output = alphabetize(input);
+//window.console.log("You entered: " + input + "\nResult: " + output);
 
 //STEP 2
 
