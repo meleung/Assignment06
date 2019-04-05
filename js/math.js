69d3b51b4ab34b56916a9fd20a2a25b347534f20
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 19:52:00 -0700

    math.js: Complete Step 4

--- js/math.js
+++ js/math.js
@@ -25,5 +25,17 @@
 //}
 
 //STEP 4
+//var input, inputParsed, i;
+//input = window.prompt("Please enter 5 numbers separated by commas.\nE.g., \"1,2,3,4,5\"").trim();
+//inputParsed = input.split(",");
+//for (i in inputParsed) {
+//    if (inputParsed.hasOwnProperty(i)) {
+//        inputParsed[i] = parseInt(inputParsed[i], 10);
+//    }
+//}
+//inputParsed = inputParsed.sort();
+//window.console.log("You entered: " + input);
+//window.console.log("The largest number is " + inputParsed[inputParsed.length - 1]);
+//window.console.log("The smallest number is " + inputParsed[0]);
 
 //STEP 5
