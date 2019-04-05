bacd820b7bc6e1228505924fbd289d23a1b5bcc9
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 19:06:46 -0700

    strings.js: Complete Step 2

--- js/strings.js
+++ js/strings.js
@@ -2,9 +2,22 @@
 
 //STEP 1
 //var name = window.prompt("Please enter you name.").trim();
-//window.alert("'" + name + "' has " + name.replace(/\W/g, "").length + " letters in it.");
+//window.alert("'" + name + "' has " + (name.replace(/(\W|\s)/g, "").length - 1) + " letters in it.");
 
 //STEP 2
+//var name = window.prompt("Please enter you name.").trim();
+//// Only counting printable characters.
+//var nameParsed = name.replace(/(\W|\s)/g, "");
+//var n = parseInt(window.prompt("Please enter a number"), 10);
+//
+//if (isNaN(n)) {
+//    window.alert("Please enter a valid number.");
+//} else if (n > (nameParsed.length - 1)) {
+//    window.alert("The name '" + name + "' does not have a letters at " + n + ".");
+//} else {
+//    window.alert("The name '" + name + "' has the letter '" + nameParsed.charAt(n) + "' at " + n + ".");
+//}
+
 
 //STEP 3
 
