afd87a337230c3393944fa2c409a1e712da678be
Matthew Leung <matthew.e.leung@gmail.com>
2019-04-04 20:20:41 -0700

    dates.js: Complete Step 1

--- js/dates.js
+++ js/dates.js
@@ -1,6 +1,24 @@
 /*eslint-env browser*/
 
 //STEP 1
+//var month, year, date, months;
+//months = [ "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+//
+//month = window.prompt("Enter a month as a digit:\nE.g., January = 1, February = 2, etc.");
+//year = window.prompt("Enter a year");
+//
+//if (isNaN(month) || isNaN(year)) {
+//    window.console.log("Please enter valid numbers for the month and year");
+//} else {
+//    month = parseInt(month, 10);
+//    if (month < 1 || month > 12) {
+//        window.console.log("Please enter a valid month.");
+//    } else {
+//        year = parseInt(year, 10);
+//        date = new Date(year, month, 0);
+//        window.console.log("There are " + date.getDate() + " days in " + months[date.getMonth()] + " " + (date.getYear() + 1900));
+//    }
+//}
 
 //STEP 2
 
